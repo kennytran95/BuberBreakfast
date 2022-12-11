f568068c3f8d6c5c0f524a7b4d78c7546d6985c6
Kenny <Kennyytran95@gmail.com>
2022-12-11 15:15:37 -0800

    create rest endpoints

--- BuberBreakfast/Controllers/BreakfastController.cs
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts.Breakfast;
 
 namespace BuberBreakfast.Controllers;
 
@@ -6,5 +7,30 @@ namespace BuberBreakfast.Controllers;
 
 public class BreakfastsController : ControllerBase
 {
+  [HttpPost("/breakfasts")]
 
+  public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+  {
+    return Ok(request);
+  }
+
+  [HttpGet("/breakfasts/{id:guid}")]
+
+  public IActionResult GetBreakfast(Guid id)
+  {
+    return Ok(id);
+  }
+
+  [HttpPut("/breakfasts/{id:guid}")]
+  public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+  {
+    return Ok(request);
+  }
+
+  [HttpDelete("/breakfasts/{id:guid}")]
+
+  public IActionResult DeleteBreakfast(Guid id)
+  {
+    return Ok(id);
+  }
 }
